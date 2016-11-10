ac4233ed6a19cd7b4217a29df42b12f06ebb2e0c
Emmanuel Madrigal <emma.madri97@gmail.com>
2016-11-10 14:24:02 -0600

    Added conditional execution of instrucions

--- processorTest.v
+++ processorTest.v
@@ -37,15 +37,15 @@ module processorTest;
         
 		// Add stimulus here
 		#10; write_ins = 0; ins_address = 0; ins = 32'b11110010100000000000000000000100;//ADD $0, $0, 4
-		#10; write_ins = 1;									//AW__opIcmd_S_Rn__Rd_/Rot/Immedia
+		#10; write_ins = 1;									//CONDopIcmd_S_Rn__Rd_/Rot/Immedia
 		#10; write_ins = 0; ins_address = 1; ins = 32'b11110010100000000001000000001000;//ADD $1, $0, 8
-		#10; write_ins = 1;									//AW__opIcmd_S_Rn__Rd_/Rot/Immedia
+		#10; write_ins = 1;									//CONDopIcmd_S_Rn__Rd_/Rot/Immedia
+		#10; write_ins = 0; ins_address = 1; ins = 32'b11110010010100000011000000000100;//SUBS $3, $0, 4
+		#10; write_ins = 1;									//CONDopIcmd_S_Rn__Rd_/Rot/Immedia
 		#10; write_ins = 0; ins_address = 2; ins = 32'b11110110000000010000000000000000;//STR $1, $0, 0
-		#10; write_ins = 1;									//AW__opI**B*L_Rn__Rd_/Immediate__
-		#10; write_ins = 0; ins_address = 2; ins = 32'b11110110000000010000000000000000;//STR $1, $0, 0
-		#10; write_ins = 1;									//AW__opI**B*L_Rn__Rd_/Immediate__
-		#10; write_ins = 0; ins_address = 3; ins = 32'b11111010111111111111111111111011;//B 0
-		#10; write_ins = 1;									//AW__op1L_______Immediate________
+		#10; write_ins = 1;									//CONDopI**B*L_Rn__Rd_/Immediate__
+		#10; write_ins = 0; ins_address = 3; ins = 32'b00001010111111111111111111111011;//BEQ 0
+		#10; write_ins = 1;									//CONDop1L_______Immediate________
 		#10; write_ins = 0;
 		#10; clk = 1;
 		#10; clk = 0;
